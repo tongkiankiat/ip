c4d2d7714349588a5ef04b769810b4f965d34423
tongkiankiat <tong.kiankiat@gmail.com>
2025-01-22 20:41:09 +0800

    Level-0: Rename, Greet, Exit

---
+++ src/main/java/Sean.java
@@ -0,0 +1,7 @@
+public class Sean {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Sean");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
