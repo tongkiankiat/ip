6c9d12c0f2a1ad0c902cef3aa02d858aff70a9cd
tongkiankiat <tong.kiankiat@gmail.com>
2025-01-30 14:58:15 +0800

    Echo

--- src/main/java/Sean.java
+++ src/main/java/Sean.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class Sean {
     public static void main(String[] args) {
+        String input;
+        Scanner in = new Scanner(System.in);
+
         System.out.println("Hello! I'm Sean");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        // Echoes user input
+        while (true) {
+            input = in.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                in.close();
+                return;
+            }
+            System.out.println(input);
+        }
     }
 }
