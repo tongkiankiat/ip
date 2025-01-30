4e53be94a58d5fc606b702d47f455c797f83fcf3
tongkiankiat <tong.kiankiat@gmail.com>
2025-01-30 15:22:00 +0800

    Add, List

--- src/main/java/Sean.java
+++ src/main/java/Sean.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Sean {
     public static void main(String[] args) {
         String input;
         Scanner in = new Scanner(System.in);
+        String[] taskList = new String[100];
+        int taskCounter = 0;
 
         System.out.println("Hello! I'm Sean");
         System.out.println("What can I do for you?");
@@ -11,12 +14,25 @@ public class Sean {
         // Echoes user input
         while (true) {
             input = in.nextLine();
+            // End conversation when user says bye
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 in.close();
                 return;
             }
-            System.out.println(input);
+
+            // Display list when asked
+            if (input.equals("list")) {
+                for (int i = 0; i < taskCounter; i++) {
+                    System.out.println(Integer.toString(i + 1) + ". " + taskList[i]);
+                }
+            }
+            // Else we store the task in the list
+            else {
+                System.out.println("added: " + input);
+                taskList[taskCounter] = input;
+                taskCounter++;
+            }
         }
     }
 }
