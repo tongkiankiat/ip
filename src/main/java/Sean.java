9292d511c1cd3b4f55447a39487b9402885aadb5
tongkiankiat <tong.kiankiat@gmail.com>
2025-02-05 17:02:30 +0800

    Improve Code Quality

--- src/main/java/Sean.java
+++ src/main/java/Sean.java
@@ -2,11 +2,13 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Sean {
+    // Initialise Array to store Tasks
+    public static Task[] taskList = new Task[100];
+    public static int taskCounter = 0;
+
     public static void main(String[] args) {
         String input;
         Scanner in = new Scanner(System.in);
-        Task[] taskList = new Task[100];
-        int taskCounter = 0;
 
         System.out.println("Hello! I'm Sean");
         System.out.println("What can I do for you?");
@@ -16,83 +18,110 @@ public class Sean {
             // End conversation when user says bye
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
-                in.close();
-                return;
+                break;
             }
 
             // Display list when asked
             if (input.equals("list")) {
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < taskCounter; i++) {
-                    System.out.println(i + 1 + "." + taskList[i].toString());
-                }
+                displayTaskList();
             }
             // Unmarking a task
             else if (input.startsWith("unmark") && input.split(" ")[1].matches("\\d+")) {
-                int taskIndex = Integer.parseInt(input.split(" ")[1]);
-                if (taskIndex > taskCounter) {
-                    System.out.println("Task number out of range!");
-                    continue;
-                }
-                if (!taskList[taskIndex - 1].isDone) {
-                    System.out.println("Task is already unmarked.");
-                    continue;
-                }
-                System.out.println("OK, I've marked this task as not done yet:");
-                taskList[taskIndex - 1].markAsUndone();
-                System.out.println("  " + taskList[taskIndex - 1].toString());
+                markTask(input, false);
             }
             // Marking a task
             else if (input.startsWith("mark") && input.split(" ")[1].matches("\\d+")) {
-                int taskIndex = Integer.parseInt(input.split(" ")[1]);
-                if (taskIndex > taskCounter) {
-                    System.out.println("Task number out of range!");
-                    continue;
-                }
-                if (taskList[taskIndex - 1].isDone) {
-                    System.out.println("Task is already marked as done.");
-                    continue;
-                }
-                System.out.println("Nice! I've marked this task as done:");
-                taskList[taskIndex - 1].markAsDone();
-                System.out.println("  " + taskList[taskIndex - 1].toString());
-
+                markTask(input, true);
             }
             // Else we move on to todo, deadline, or task prompts
             else {
                 if (input.startsWith("todo")) {
-                    String todoTask = input.split("todo")[1].strip();
-                    taskList[taskCounter] = new Todo(todoTask, todoTask);
-                    taskCounter++;
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println("  [T][ ] " + todoTask);
-                    System.out.println("Now you have " + taskCounter + (taskCounter == 1 ? " task " : " tasks ") + "in the list.");
+                    addTodoTask(input);
                 }
                 else if (input.startsWith("deadline")) {
-                    String deadlineTask = input.split("deadline | /by")[1].strip();
-                    String deadlineBy = input.split("/by")[1].strip();
-                    taskList[taskCounter] = new Deadline(deadlineTask, deadlineBy);
-                    taskCounter++;
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println("  [D][ ] " + deadlineTask + " (by: " + deadlineBy + ")");
-                    System.out.println("Now you have " + taskCounter + (taskCounter == 1 ? " task " : " tasks ") + "in the list.");
+                    addDeadlineTask(input);
                 }
                 else if (input.startsWith("event")) {
-                    String eventTask = input.split("event | /from | /to")[1].strip();
-                    String eventFrom = input.split("/from | /to")[1].strip();
-                    String eventTo = input.split("/to")[1].strip();
-                    taskList[taskCounter] = new Event(eventTask, eventFrom, eventTo);
-                    taskCounter++;
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println("  [E][ ] " + eventTask + " (from: " + eventFrom + " to: " + eventTo + ")");
-                    System.out.println("Now you have " + taskCounter + " tasks in the list.");
+                    addEventTask(input);
                 }
                 else {
-                    System.out.println("added: " + input);
-                    taskList[taskCounter] = new Task(input);
-                    taskCounter++;
+                    addNormalTask(input);
                 }
             }
         }
+        in.close();
+    }
+
+    // Display task list
+    public static void displayTaskList() {
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < taskCounter; i++) {
+            System.out.println(i + 1 + "." + taskList[i].toString());
+        }
+    }
+
+    // Marking/Unmarking Task
+    public static void markTask(String input, boolean isDone) {
+        int taskIndex = Integer.parseInt(input.split(" ")[1]);
+
+        if (taskIndex < 0 || taskIndex > taskCounter) {
+            System.out.println("Task number out of range!");
+            return;
+        }
+        if (taskList[taskIndex - 1].isDone == isDone) {
+            System.out.println((isDone ? "Task is already marked." : "Task is already unmarked"));
+            return;
+        }
+
+        System.out.println(isDone ? "Nice! I've marked this task as done:" : "OK, I've marked this task as not done yet:");
+
+        if (isDone) {
+            taskList[taskIndex - 1].markAsDone();
+        } else {
+            taskList[taskIndex - 1].markAsUndone();
+        }
+
+        System.out.println("  " + taskList[taskIndex - 1].toString());
+    }
+
+    // Add todo task
+    public static void addTodoTask(String input) {
+        String todoTask = input.split("todo")[1].strip();
+        taskList[taskCounter] = new Todo(todoTask, todoTask);
+        taskCounter++;
+        printTaskAdded("  [T][ ] " + todoTask);
+    }
+
+    // Add deadline task
+    public static void addDeadlineTask(String input) {
+        String deadlineTask = input.split("deadline | /by")[1].strip();
+        String deadlineBy = input.split("/by")[1].strip();
+        taskList[taskCounter] = new Deadline(deadlineTask, deadlineBy);
+        taskCounter++;
+        printTaskAdded("  [D][ ] " + deadlineTask + " (by: " + deadlineBy + ")");
+    }
+
+    // Add event task
+    public static void addEventTask(String input) {
+        String eventTask = input.split("event | /from | /to")[1].strip();
+        String eventFrom = input.split("/from | /to")[1].strip();
+        String eventTo = input.split("/to")[1].strip();
+        taskList[taskCounter] = new Event(eventTask, eventFrom, eventTo);
+        taskCounter++;
+        printTaskAdded("  [E][ ] " + eventTask + " (from: " + eventFrom + " to: " + eventTo + ")");
+    }
+
+    // Add normal task
+    public static void addNormalTask(String input) {
+        taskList[taskCounter] = new Task(input);
+        taskCounter++;
+        System.out.println("added: " + input);
+    }
+
+    // Print task added
+    public static void printTaskAdded(String uniqueMessage) {
+        System.out.println("Got it. I've added this task:");
+        System.out.println(uniqueMessage);
+        System.out.println("Now you have " + taskCounter + (taskCounter == 1 ? " task " : " tasks ") + "in the list.");
     }
 }
